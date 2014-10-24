8bf7158f9aa3623d12dccbf6f0de43fe1c7ba4e3
Ashish Meena <ashish@rti.com>
2014-10-24 18:13:22 +0100

    update build.sh and subscriber

--- rpi_ultrasonic/rpi_ultrasonic_C/build.sh
+++ rpi_ultrasonic/rpi_ultrasonic_C/build.sh
@@ -3,10 +3,10 @@ echo "Generating Type Support code from IDL \n"
 rtiddsgen -replace -language C ShapeType.idl
 
 echo " Building the source code \n"
-make -f makefile_BMP_temp_pressure_armv6vfphLinux3.xgcc4.7.2
+make -f makefile_ShapeType_armv6vfphLinux3.xgcc4.7.2
 
 echo "Runnign the appication, pass sudo password where needed\n"
-./objs/armv6vfphLinux3.xgcc4.7.2/ShapeType_publisher
+sudo ./objs/armv6vfphLinux3.xgcc4.7.2/ShapeType_publisher
 
 
 echo "Use Android DDS ShapesDemo app and subscribe to a RED Square\n"
