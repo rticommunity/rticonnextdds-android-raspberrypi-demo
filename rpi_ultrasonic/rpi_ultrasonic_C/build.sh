f6c883e6f735556359c6cb7f27c41170c938de4e
Ashish Meena <ashish@rti.com>
2014-10-24 17:49:49 +0100

    add buil.sh and subscriber app

---
+++ rpi_ultrasonic/rpi_ultrasonic_C/build.sh
@@ -0,0 +1,17 @@
+
+echo "Generating Type Support code from IDL \n"
+rtiddsgen -replace -language C ShapeType.idl
+
+echo " Building the source code \n"
+make -f makefile_BMP_temp_pressure_armv6vfphLinux3.xgcc4.7.2
+
+echo "Runnign the appication, pass sudo password where needed\n"
+./objs/armv6vfphLinux3.xgcc4.7.2/ShapeType_publisher
+
+
+echo "Use Android DDS ShapesDemo app and subscribe to a RED Square\n"
+echo "Alternatively on the other terminal run the subscriber applicatio \n"
+echo "$.objs/armv6vfphLinux3.xgcc4.7.2/ShapeType_subscriber\n"
+
+
+
