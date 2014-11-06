5ba760ca211ef38626579effb98c882771b46147
Ashish Meena <ashish@rti.com>
2014-11-06 12:32:08 +0100

    New files added for Webcam view integration

---
+++ rpi_stepper_motors/android_motor_control_app/src/com/camera/simplemjpeg/MjpegActivity.java
@@ -0,0 +1,249 @@
+package com.camera.simplemjpeg;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+
+import com.rti.motorcontrolpub.MotorControlMainActivity;
+import com.rti.motorcontrolpub.R;
+
+
+public class MjpegActivity extends Activity {
+	private static final boolean DEBUG=false;
+    private static final String TAG = "MJPEG";
+
+
+  // public MjpegView mv = null; //using Global mv and URL instead in MotorControlPub.java 
+   //public String URL=null; 
+    
+    // for settings (network and resolution)
+    private static final int REQUEST_SETTINGS = 0;
+    
+    private int width = 320;
+    private int height = 240;
+    
+    private int ip_ad1 = 87;
+    private int ip_ad2 = 82;
+    private int ip_ad3 = 193;
+    private int ip_ad4 = 136;
+    private int ip_port = 8081;
+    private String ip_command = "?action=stream";
+    
+    private boolean suspending = false;
+    
+	final Handler handler = new Handler();
+ 
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+       SharedPreferences preferences = getSharedPreferences("SAVED_VALUES", MODE_PRIVATE);
+        width = preferences.getInt("width", width);
+        height = preferences.getInt("height", height);
+        ip_ad1 = preferences.getInt("ip_ad1", ip_ad1);
+        ip_ad2 = preferences.getInt("ip_ad2", ip_ad2);
+        ip_ad3 = preferences.getInt("ip_ad3", ip_ad3);
+        ip_ad4 = preferences.getInt("ip_ad4", ip_ad4);
+        ip_port = preferences.getInt("ip_port", ip_port);
+        ip_command = preferences.getString("ip_command", ip_command);
+                
+        StringBuilder sb = new StringBuilder();
+        String s_http = "http://";
+        String s_dot = ".";
+        String s_colon = ":";
+        String s_slash = "/";
+        sb.append(s_http);
+        sb.append(ip_ad1);
+        sb.append(s_dot);
+        sb.append(ip_ad2);
+        sb.append(s_dot);
+        sb.append(ip_ad3);
+        sb.append(s_dot);
+        sb.append(ip_ad4);
+        sb.append(s_colon);
+        sb.append(ip_port);
+        sb.append(s_slash);
+        sb.append(ip_command);
+        MotorControlMainActivity.URL = new String(sb);
+
+       //setContentView(R.layout.main);
+        MotorControlMainActivity.mv = (MjpegView) findViewById(R.id.mv);  
+      if(MotorControlMainActivity.mv != null){
+    	  MotorControlMainActivity.mv.setResolution(width, height);
+       }
+        
+      //setTitle(R.string.title_connecting);
+     // new DoRead().execute(MotorControlMainActivity.URL);
+    }
+
+    
+  public void onResume() {
+    	if(DEBUG) Log.d(TAG,"onResume()");
+        super.onResume();
+        if(MotorControlMainActivity.mv!=null){
+        	if(suspending){
+        		new DoRead().execute(MotorControlMainActivity.URL);
+        		suspending = false;
+        	}
+        }
+
+    }
+
+    public void onStart() {
+    	if(DEBUG) Log.d(TAG,"onStart()");
+        super.onStart();
+    }
+    
+    public void onPause() {
+    	if(DEBUG) Log.d(TAG,"onPause()");
+        super.onPause();
+        if(MotorControlMainActivity.mv!=null){
+        	if(MotorControlMainActivity.mv.isStreaming()){
+        		MotorControlMainActivity.mv.stopPlayback();
+		        suspending = true;
+        	}
+        }
+    }
+    public void onStop() {
+    	if(DEBUG) Log.d(TAG,"onStop()");
+        super.onStop();
+    }
+
+    public void onDestroy() {
+    	if(DEBUG) Log.d(TAG,"onDestroy()");
+    	
+    	if(MotorControlMainActivity.mv!=null){
+    		MotorControlMainActivity.mv.freeCameraMemory();
+    	}
+    	
+        super.onDestroy();
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.layout.option_menu, menu);
+    	return true;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	switch (requestCode) {
+    		case REQUEST_SETTINGS:
+    			if (resultCode == Activity.RESULT_OK) {
+    				width = data.getIntExtra("width", width);
+    				height = data.getIntExtra("height", height);
+    				ip_ad1 = data.getIntExtra("ip_ad1", ip_ad1);
+    				ip_ad2 = data.getIntExtra("ip_ad2", ip_ad2);
+    				ip_ad3 = data.getIntExtra("ip_ad3", ip_ad3);
+    				ip_ad4 = data.getIntExtra("ip_ad4", ip_ad4);
+    				ip_port = data.getIntExtra("ip_port", ip_port);
+    				ip_command = data.getStringExtra("ip_command");
+
+    				if(MotorControlMainActivity.mv!=null){
+    					MotorControlMainActivity.mv.setResolution(width, height);
+    				}
+    				SharedPreferences preferences = getSharedPreferences("SAVED_VALUES", MODE_PRIVATE);
+    				SharedPreferences.Editor editor = preferences.edit();
+    				editor.putInt("width", width);
+    				editor.putInt("height", height);
+    				editor.putInt("ip_ad1", ip_ad1);
+    				editor.putInt("ip_ad2", ip_ad2);
+    				editor.putInt("ip_ad3", ip_ad3);
+    				editor.putInt("ip_ad4", ip_ad4);
+    				editor.putInt("ip_port", ip_port);
+    				editor.putString("ip_command", ip_command);
+
+    				editor.commit();
+
+    				new RestartApp().execute();
+    			}
+    			break;
+    	}
+    }
+
+    public void setImageError(){
+    	handler.post(new Runnable() {
+    		@Override
+    		public void run() {
+    			setTitle(R.string.title_imageerror);
+    			return;
+    		}
+    	});
+    }
+    
+    public class DoRead extends AsyncTask<String, Void, MjpegInputStream> {
+        protected MjpegInputStream doInBackground(String... url) {
+            //TODO: if camera has authentication deal with it and don't just not work
+            HttpResponse res = null;         
+            DefaultHttpClient httpclient = new DefaultHttpClient(); 
+            HttpParams httpParams = httpclient.getParams();
+            HttpConnectionParams.setConnectionTimeout(httpParams, 5*1000);
+            HttpConnectionParams.setSoTimeout(httpParams, 5*1000);
+            if(DEBUG) Log.d(TAG, "1. Sending http request");
+            try {
+                res = httpclient.execute(new HttpGet(URI.create(url[0])));
+                if(DEBUG) Log.d(TAG, "2. Request finished, status = " + res.getStatusLine().getStatusCode());
+                if(res.getStatusLine().getStatusCode()==401){
+                    //You must turn off camera User Access Control before this will work
+                    return null;
+                }
+                return new MjpegInputStream(res.getEntity().getContent());  
+            } catch (ClientProtocolException e) {
+            	if(DEBUG){
+	                e.printStackTrace();
+	                Log.d(TAG, "Request failed-ClientProtocolException", e);
+            	}
+                //Error connecting to camera
+            } catch (IOException e) {
+            	if(DEBUG){
+	                e.printStackTrace();
+	                Log.d(TAG, "Request failed-IOException", e);
+            	}
+                //Error connecting to camera
+            }
+            return null;
+        }
+
+        protected void onPostExecute(MjpegInputStream result) {
+        	MotorControlMainActivity.mv.setSource(result);
+            if(result!=null){
+            	result.setSkip(1);
+            	//setTitle(R.string.app_name);
+            }else{
+            	setTitle(R.string.title_disconnected);
+            }
+            MotorControlMainActivity.mv.setDisplayMode(MjpegView.SIZE_BEST_FIT);
+            MotorControlMainActivity.mv.showFps(false);
+        }
+ 
+    }
+    
+    public class RestartApp extends AsyncTask<Void, Void, Void> {
+        protected Void doInBackground(Void... v) {
+        	MjpegActivity.this.finish();
+            return null;
+        }
+
+        protected void onPostExecute(Void v) {
+        	startActivity((new Intent(MjpegActivity.this, MjpegActivity.class)));
+        }
+    }
+}
