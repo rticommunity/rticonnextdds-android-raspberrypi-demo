4100825982909955f5717aef89e0853b8aec4033
Ashish Meena <ashish@rti.com>
2014-11-21 16:16:19 +0000

    Update WEb DDS related code section

--- rpi_stepper_motors/android_motor_control_app/src/com/rti/motorcontrolpub/MotorControlPublisher.java
+++ rpi_stepper_motors/android_motor_control_app/src/com/rti/motorcontrolpub/MotorControlPublisher.java
@@ -84,7 +84,6 @@ public class MotorControlPublisher {
  	public static Boolean flagBtn = false;
 	
 	public static int Pub_sub_create_count=1;
-	public static int Pub_sub_create_count_http=1;
 
 	public static void main(String[] args) {
 		// --- Get domain ID --- //
